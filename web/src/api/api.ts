b85e2876c51d916844c5056fff171a7816068b54
Luc <luc@lucemans.nl>
2025-03-17 15:04:50 +0100

    Update origin

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,6 +1,6 @@
 import { createFetch } from "openapi-hooks";
 import { paths } from "./schema.gen";
 
-const baseUrl = new URL('http://localhost:5173/api/');
+const baseUrl = new URL('/api/', window.location.origin);
 
 export const useApi = createFetch<paths>({ baseUrl });
